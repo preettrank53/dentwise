92a0e26b4741910fd14ee0d05981783fd2376408
Preet Rank <preetrank53@gmail.com>
2026-04-25 20:52:18 +0530

    fix: refactor metadataBase URL assignment for improved readability

--- src/app/layout.js
+++ src/app/layout.js
@@ -45,8 +45,11 @@ export const metadata = {
   creator: 'Dentwise',
   publisher: 'Dentwise',
   metadataBase: new URL(
-    process.env.NEXTAUTH_URL || 
-    (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000')
+    process.env.NEXTAUTH_URL
+      ? process.env.NEXTAUTH_URL
+      : process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}`
+      : 'http://localhost:3000'
   ),
   openGraph: {
     type: 'website',
